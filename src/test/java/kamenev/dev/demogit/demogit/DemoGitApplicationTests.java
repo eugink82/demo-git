a7949c38a48f40406d459b74a284d16f668aad88
Евгений <eugink82@mail.ru>
2025-10-07 20:16:22 +0300

    init commit

---
+++ src/test/java/kamenev/dev/demogit/demogit/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package kamenev.dev.demogit.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
